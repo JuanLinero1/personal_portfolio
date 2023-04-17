48b32e2013c8d60b20f0eb2a059bc63836580662
juanLinero <juanlinerobonnett611@gmail.com>
2023-04-17 10:22:31 -0500

    completed header, if i want i will add some changes lates.

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFile,
+  faFolder,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  const [headerClass, setHeaderClass] = useState(false);
+
+  const changeCondition = () => {
+    window.scrollY > 150 ? setHeaderClass(true) : setHeaderClass(false);
+  };
+  window.addEventListener("scroll", changeCondition);
+
+  return (
+    <header className={headerClass ? "header active" : "header"}>
+      <div className="header-logo">ALB</div>
+      <nav className="header__nav">
+        <ul className="header__nav--list">
+          <a href="#">
+            <li className="header__nav--list-item">
+              <FontAwesomeIcon icon={faHouse} />
+              Home
+            </li>
+          </a>
+          <a href="#">
+            <li className="header__nav--list-item">
+              <FontAwesomeIcon icon={faUser} />
+              About
+            </li>
+          </a>
+          <a href="#">
+            <li className="header__nav--list-item">
+              <FontAwesomeIcon icon={faFolder} />
+              Project
+            </li>
+          </a>
+          <a href="#">
+            <li className="header__nav--list-item">
+              <FontAwesomeIcon icon={faFile} />
+              Resume
+            </li>
+          </a>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
