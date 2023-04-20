a2f446ddba7c2b287da0b9784883979bca1fcee1
juanLinero <juanlinerobonnett611@gmail.com>
2023-04-20 15:52:27 -0500

    resume section completed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHouse,
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 const Resume = () => {
   return (
-    <div>Resume</div>
-  )
-}
+    <div className="resume">
+      <a
+        href="../../../public/assets/images/CvBonnett.pdf"
+        download={"CvBonnett"}
+      >
+        <button className="resume-button">
+          Download <FontAwesomeIcon icon={faDownload} />{" "}
+        </button>
+      </a>
+      <embed src="../../../public/assets/images/CvBonnett.pdf" type="application/pdf" width="608px" height="857px"></embed>
+    </div>
+  );
+};
 
-export default Resume
+export default Resume;
